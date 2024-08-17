9c7a243486783f3285487cf14ea6ce0d007fd50c
amine belfkira <amine.belfkira99@gmail.com>
2024-08-17 18:18:17 +0100

    high score

--- game/scoreboard.py
+++ game/scoreboard.py
@@ -1,8 +1,10 @@
 from turtle import Turtle
+import os
 
 COLOR = 'White'
 FONT = ("Arial", 24, "normal")
 ALIGN = "center"
+HIGH_SCORE_FILE = "high_score.txt"
 class Scoreboard(Turtle):
     
     def __init__(self) -> None:
@@ -12,13 +14,30 @@ class Scoreboard(Turtle):
         self.penup()
         self.goto(0, 270)
         self.hideturtle()
+        self.hight_score = self.get_highest_score()
         self.show_score()
 
     def show_score(self):
-        self.write(f"Score = {self.score}", align=ALIGN, font=FONT)
+        self.write(f"Score = {self.score} | Highest Score = {self.hight_score}", align=ALIGN, font=FONT)
+     
+    def get_highest_score(self):
+        if os.path.isfile(HIGH_SCORE_FILE):
+            with open(HIGH_SCORE_FILE, 'r') as f:
+                line = f.read().strip()
+        try:
+            return int(line)
+        except ValueError:
+            return 0
         
+    def new_highest_score(self, new_score):
+        self.hight_score = new_score
+        with open(HIGH_SCORE_FILE, 'r') as f:
+            f.write(str(new_score))
+           
     def increase_score(self):
         self.score += 1
+        if self.score > self.hight_score:
+            self.new_highest_score(self.score)
         self.clear()
         self.show_score()
         
@@ -26,4 +45,5 @@ class Scoreboard(Turtle):
         self.clear()
         self.goto(0, 0)
         self.write(f"GAME OVER", align=ALIGN, font=FONT)
+        
         
